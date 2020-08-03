c24acfa45fc7057aafa908c78a6f03b5d50fa86c
Lucas dos Santos de Souza Tramontin <hilucas42@gmail.com>
2020-08-03 02:42:12 -0300

    Fourth class - Heroku API added

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,53 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import DefaultTemplate from '../../../components/DefaultTemplate';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-  const emBranco = { nome: '', descricao: '', cor: '#000000'};
+  const emBranco = { nome: '', descricao: '', cor: '#000000' };
   const [novaCategoria, setNovaCategoria] = useState(emBranco);
   const [categorias, setCategorias] = useState([]);
 
   function handleChange(e) {
     setNovaCategoria({
       ...novaCategoria,
-      [e.target.getAttribute('name')]: e.target.value
-    })
+      [e.target.getAttribute('name')]: e.target.value,
+    });
   }
+
+  useEffect(() => {
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://notflix-unlicensed.herokuapp.com/categorias';
+    fetch(URL)
+      .then(async (resposta) => setCategorias(await resposta.json()));
+  }, []);
 
   return (
     <DefaultTemplate>
       <h1>Cadastro de categoria</h1>
 
-      <form onSubmit={e => {
+      <form onSubmit={(e) => {
         e.preventDefault();
         setCategorias([...categorias, novaCategoria]);
         setNovaCategoria(emBranco);
-      }}>
-        
+      }}
+      >
+
         <FormField
-          name="nome"
+          name="titulo"
           type="text"
           label="Categoria: "
-          value={novaCategoria.nome}
+          value={novaCategoria.titulo}
           onChange={handleChange}
         />
 
         <FormField
-          name = "descricao"
-          type = "textarea"
-          label = "Descrição: "
+          name="descricao"
+          type="textarea"
+          label="Descrição: "
           value={novaCategoria.descricao}
-          onChange = {handleChange}
+          onChange={handleChange}
         />
 
         <FormField
-          name = "cor"
-          type = "color"
-          label = "Cor: "
+          name="cor"
+          type="color"
+          label="Cor: "
           value={novaCategoria.cor}
-          onChange = {handleChange}r
+          onChange={handleChange}
         />
 
         <Button>
@@ -55,17 +64,21 @@ function CadastroCategoria() {
         </Button>
       </form>
 
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+
       <ul>
-        {categorias.map((categoria, index) => {
-          return (
-            <li key={index}>
-              {categoria.nome}
-            </li>
-          )
-        })}
+        {categorias.map((categoria) => (
+          <li key={categoria.id}>
+            {categoria.titulo}
+          </li>
+        ))}
       </ul>
 
-      <Link to='/cadastro/video'>
+      <Link to="/cadastro/video">
         Cadastrar vídeo
       </Link>
     </DefaultTemplate>
